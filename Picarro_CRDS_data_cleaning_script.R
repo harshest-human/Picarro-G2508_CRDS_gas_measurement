11d3ff64de56f973bba20befb4c437bbc1553625
Harsh Sahu <harshsahu.8085@gmail.com>
2024-06-12 12:20:59 +0200

    merge G2508 and G2509

--- Picarro_CRDS_data_cleaning_script.R
+++ Picarro_CRDS_data_cleaning_script.R
@@ -5,7 +5,6 @@ library(progress)
 library(ggplot2)
 library(ggpubr)
 
-
 ####### Development of function ########
 # Function to merge data files
 mergeDATFiles <- function(input_path, output_path, result_file_name) {
@@ -50,41 +49,132 @@ mergeDATFiles <- function(input_path, output_path, result_file_name) {
 
 
 # Example usage
-#input_path <- "D:/Data Analysis/Gas_data/Raw_data/CRDS_raw"
+#input_path <- "D:/Data Analysis/Gas_data/Raw_data/CRDS_raw/Picarro_G2508"
 #output_path <- "D:/Data Analysis/Gas_data/Clean_data/CRDS_clean"
-#result_file_name <- "2023-12-19_2024-01-22_picarro_data"
+#result_file_name <- "2024-06-03_2024-06-11_Picarro08"
 #CRDS_data <- mergeDATFiles(input_path, output_path, result_file_name)
 
 
-####### Data Processing ########
+####### Picarro G2508 Data Processing ########
+input_path <- "D:/Data Analysis/Gas_data/Raw_data/CRDS_raw/Picarro_G2508"
+output_path <- "D:/Data Analysis/Gas_data/Clean_data/CRDS_clean"
+result_file_name <- "2024-06-03_2024-06-11_Picarro08"
+CRDS.P8_data <- mergeDATFiles(input_path, output_path, result_file_name)
+
 # import the dataset
-CRDS_data <- read.table("D:/Data Analysis/Gas_data/Clean_data/CRDS_clean/2023-12-19_2024-01-22_picarro_data.dat", sep="", header = TRUE)
+CRDS.P8_data <- read.csv("D:/Data Analysis/Gas_data/Clean_data/CRDS_clean/2024-06-03_2024-06-11_Picarro08.dat", sep="")
+
+# Find the number of non-integer values in MPVPosition
+num_non_integers <- sum(!is.na(CRDS.P8_data$MPVPosition) & (CRDS.P8_data$MPVPosition %% 1 != 0))
+
+# Find the number of zero values in MPVPosition
+num_zeros <- sum(!is.na(CRDS.P8_data$MPVPosition) & CRDS.P8_data$MPVPosition == 0)
+
+# Print the results
+cat("Number of non-integer values in MPVPosition:", num_non_integers, "\n")
+cat("Number of zero values in MPVPosition:", num_zeros, "\n")
 
 # Remove non-integer values from MPVPosition and convert to factor
-CRDS_data <-  filter(CRDS_data, MPVPosition %% 1 == 0 & MPVPosition != 0)
+CRDS.P8_data <-  filter(CRDS.P8_data, MPVPosition %% 1 == 0 & MPVPosition != 0)
 
-# Convert N2O, CO2, CH4, H2O, NH3 to numeric
+# Convert charachters into numeric and factors 
 numeric_columns <- c("N2O", "CO2", "CH4", "H2O", "NH3")
-CRDS_data[, numeric_columns] <- apply(CRDS_data[, numeric_columns], 2, as.numeric)
+CRDS.P8_data[, numeric_columns] <- apply(CRDS.P8_data[, numeric_columns], 2, as.numeric)
+CRDS.P8_data$MPVPosition <- as.factor(CRDS.P8_data$MPVPosition)
 
 # Merge DATE and TIME to Date.time and format as yyyy-mm-dd HH:MM:SS
-CRDS_data$Date.Time <- as.POSIXct(paste(CRDS_data$DATE, CRDS_data$TIME), format = "%Y-%m-%d %H:%M:%S")
+CRDS.P8_data$Date.Time <- as.POSIXct(paste(CRDS.P8_data$DATE, CRDS.P8_data$TIME), format = "%Y-%m-%d %H:%M:%S")
 
 # Select specific columns
-CRDS_data <- select(CRDS_data, Date.Time, MPVPosition, N2O, CO2, CH4, H2O, NH3,-c(DATE, TIME))
+CRDS.P8_data <- select(CRDS.P8_data, Date.Time, MPVPosition, N2O, CO2, CH4, H2O, NH3)
 
-#Remove outliers (1.5 IQR)
-remove_outliers_function <- source("remove_outliers_function.R")
-CRDS_data$CO2 <- remove_outliers(CRDS_data$CO2)
-CRDS_data$CH4 <- remove_outliers(CRDS_data$CH4)
-CRDS_data$NH3 <- remove_outliers(CRDS_data$NH3)
+CRDS.P8_June <- CRDS.P8_data %>%
+        filter(Date.Time > as.POSIXct("2024-06-03 15:13:00") & Date.Time < as.POSIXct("2024-06-11 10:45:00"))
+
+
+####### Picarro G2509 Data Processing ########
+input_path <- "D:/Data Analysis/Gas_data/Raw_data/CRDS_raw/Picarro_G2509"
+output_path <- "D:/Data Analysis/Gas_data/Clean_data/CRDS_clean"
+result_file_name <- "2024-06-03_2024-06-11_Picarro09"
+CRDS.P9_data <- mergeDATFiles(input_path, output_path, result_file_name)
+
+
+####### Data Processing ########
+
+# import the dataset
+CRDS.P9_data <- read.csv("D:/Data Analysis/Gas_data/Clean_data/CRDS_clean/2024-06-03_2024-06-11_Picarro09.dat", sep="")
+
+# Find the number of non-integer values in MPVPosition
+num_non_integers <- sum(!is.na(CRDS.P9_data$MPVPosition) & (CRDS.P9_data$MPVPosition %% 1 != 0))
+
+# Find the number of zero values in MPVPosition
+num_zeros <- sum(!is.na(CRDS.P9_data$MPVPosition) & CRDS.P9_data$MPVPosition == 0)
+
+# Print the results
+cat("Number of non-integer values in MPVPosition:", num_non_integers, "\n")
+cat("Number of zero values in MPVPosition:", num_zeros, "\n")
+
+# Remove non-integer values from MPVPosition and convert to factor
+CRDS.P9_data <-  filter(CRDS.P9_data, MPVPosition %% 1 == 0 & MPVPosition != 0)
+
+# Convert charachters into numeric and factors 
+numeric_columns <- c("N2O", "CO2", "CH4", "H2O", "NH3")
+CRDS.P9_data[, numeric_columns] <- apply(CRDS.P9_data[, numeric_columns], 2, as.numeric)
+CRDS.P9_data$MPVPosition <- as.factor(CRDS.P9_data$MPVPosition)
+
+# Merge DATE and TIME to Date.time and format as yyyy-mm-dd HH:MM:SS
+CRDS.P9_data$Date.Time <- as.POSIXct(paste(CRDS.P9_data$DATE, CRDS.P9_data$TIME), format = "%Y-%m-%d %H:%M:%S")
+
+# Select specific columns
+CRDS.P9_data <- select(CRDS.P9_data, Date.Time, MPVPosition, N2O, CO2, CH4, H2O, NH3)
+
+CRDS.P9_June <- CRDS.P9_data %>%
+        filter(Date.Time > as.POSIXct("2024-06-03 15:13:00") & Date.Time < as.POSIXct("2024-06-11 10:49:00"))
+
+
+# Rename columns of CRDS.P8_June
+CRDS.P8_June <- CRDS.P8_June %>%
+        rename_with(~paste0(., ".P8"), -Date.Time)
+
+# Rename columns of CRDS.P9_June
+CRDS.P9_June <- CRDS.P9_June %>%
+        rename_with(~paste0(., ".P9"), -Date.Time)
+
+# Merge the two data frames by Date.Time
+CRDS_June.comb <- full_join(CRDS.P8_June, CRDS.P9_June, by = "Date.Time") %>%
+        mutate(across(ends_with(".P8"), ~coalesce(., NA)),
+               across(ends_with(".P9"), ~coalesce(., NA)))
+
+# Convert Date.Time to POSIXct format
+CRDS_June.comb$Date.Time <- as.POSIXct(CRDS_June.comb$Date.Time)
+
+# Round Date.Time to the nearest 4-minute interval
+CRDS_June.comb <- CRDS_June.comb %>%
+        mutate(Group = round(as.numeric(Date.Time - min(Date.Time)) / (4 * 60))) %>%
+        group_by(Group) %>%
+        summarise(across(-Date.Time, ~ ifelse(all(is.na(.)), NA, mean(., na.rm = TRUE))))
+
+# Fill NA values for the first minute
+CRDS_June.comb[1, -1] <- colMeans(CRDS_June.comb[1:4, -1], na.rm = TRUE)
 
 
 ####### Data Analysis ########
 
+#Remove outliers (1.5 IQR)
+#remove_outliers_function <- source("remove_outliers_function.R")
+#CRDS.P8_data$CO2 <- remove_outliers(CRDS.P8_data$CO2)
+#CRDS.P8_data$CH4 <- remove_outliers(CRDS.P8_data$CH4)
+#CRDS.P8_data$NH3 <- remove_outliers(CRDS.P8_data$NH3)
+
 # Plotting using ggplot2 
-CRDS_data$MPVPosition <- as.factor(CRDS_data$MPVPosition)
-ggplot(CRDS_data, aes(x=MPVPosition, y=CO2)) + geom_boxplot()
+ggplot(CRDS.P8_June, aes(x=MPVPosition, y=CO2)) + geom_boxplot()
 
 # Plotting using ggline
-ggline(CRDS_data, x="MPVPosition", y="CO2", add = "mean_se")
+ggline(CRDS.P8_June, x="MPVPosition", y="CO2", add = "mean_se")
+
+# Plotting using ggplot2 
+ggplot(CRDS.P9_June, aes(x=MPVPosition, y=CO2)) + geom_boxplot()
+
+# Plotting using ggline
+ggline(CRDS.P9_June, x="MPVPosition", y="CO2", add = "mean_se")
+
