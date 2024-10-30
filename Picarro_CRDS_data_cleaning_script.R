5ad86caef91b0fef8853445f5e40f742cef87e00
Harsh Sahu <harshsahu.8085@gmail.com>
2024-10-30 13:29:33 +0100

    piclean with step wise cat progress

--- Picarro_CRDS_data_cleaning_script.R
+++ Picarro_CRDS_data_cleaning_script.R
@@ -47,17 +47,26 @@ piclean <- function(input_path, output_path, result_file_name, columns) {
         cat("Merging DATE and TIME column...\n")
         merged_data <- merged_data %>%
                 mutate(DATE.TIME = as.POSIXct(paste(DATE, TIME), format = "%Y-%m-%d %H:%M:%S")) %>%
-                select(-DATE, -TIME,)  # Remove DATE and TIME columns
+                select(-DATE, -TIME)  # Remove DATE and TIME columns
         
-        # Print non-integer MPVPosition values
+        # Check for non-integer MPVPosition values
         non_integer_mpv <- sum(merged_data$MPVPosition %% 1 != 0)
         total_mpv <- nrow(merged_data)
-        non_integer_percentage <- (non_integer_mpv / total_mpv) * 100
         
-        cat("Non-integer MPVPosition values removed =", non_integer_mpv, "/", total_mpv, "(", round(non_integer_percentage, 2), "%)", "\n")
+        cat("Removing non-integer MPVPosition values...\n")
         
-        # Remove non-integer MPVPosition values
-        merged_data <- merged_data %>% filter(MPVPosition %% 1 == 0)
+        # Print the count and percentage of non-integer MPVPosition values
+        if (non_integer_mpv > 0) {
+                non_integer_percentage <- (non_integer_mpv / total_mpv) * 100
+                cat("Non-integer MPVPosition values found =", non_integer_mpv, "/", total_mpv, "(", round(non_integer_percentage, 2), "%)", "\n")
+                
+                # Remove non-integer MPVPosition values
+                merged_data <- merged_data %>% filter(MPVPosition %% 1 == 0)
+                
+                cat("Non-integer MPVPosition values removed. Remaining rows:", nrow(merged_data), "\n")
+        } else {
+                cat("No non-integer MPVPosition values found.\n")
+        }
         
         # Check number of observations
         n_obs <- nrow(merged_data)
@@ -66,6 +75,7 @@ piclean <- function(input_path, output_path, result_file_name, columns) {
         full_output_path <- file.path(output_path, paste0(result_file_name, ".dat"))
         
         # Write final processed data to .dat file
+        cat("Exporting the final dataframe...\n")
         write.table(merged_data, full_output_path, row.names = FALSE, sep = "\t", quote = FALSE)
         
         # Print final message
@@ -78,9 +88,7 @@ piclean <- function(input_path, output_path, result_file_name, columns) {
 
 
 
+
 #### Example usage
-piclean(input_path = "D:/Data Analysis/Gas_data/Raw_data/CRDS_raw/Picarro_G2508/2024",
-        output_path = "D:/Data Analysis/Gas_data/Clean_data/CRDS_clean",
-        result_file_name = "2024_June-Sep_CRDS.P8",
-        columns = c("DATE", "TIME", "MPVPosition", "OutletValve", "N2O", "CO2", "CH4", "H2O", "NH3"))  
+#piclean(input_path = "D:/Data Analysis/Gas_data/Raw_data/CRDS_raw/Picarro_G2508/2024", output_path = "D:/Data Analysis/Gas_data/Clean_data/CRDS_clean", result_file_name = "test_CRDS.P8", columns = c("DATE", "TIME", "MPVPosition", "OutletValve", "N2O", "CO2", "CH4", "H2O", "NH3"))  
 
