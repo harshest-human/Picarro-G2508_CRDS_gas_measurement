b070bd47c8dc8d49116ff41433c17cab6eadf607
Harsh Sahu <harshsahu.8085@gmail.com>
2024-11-11 13:34:17 +0100

    2024_Nov_06_to_11_CRDS.comb

--- Picarro_CRDS_data_analysis_script.R
+++ Picarro_CRDS_data_analysis_script.R
@@ -14,13 +14,13 @@ source("Picarro_CRDS_data_cleaning_script.R")
 
 ####### Data importing and cleaning ########
 #Picarro G2508 
-piclean(input_path = "D:/Data Analysis/Gas_data/Raw_data/CRDS_raw/Picarro_G2508/2024", output_path = "D:/Data Analysis/Gas_data/Clean_data/CRDS_clean", result_file_name = "2024_June-Sep_CRDS.P8")  
-CRDS.P8 <- fread("D:/Data Analysis/Gas_data/Clean_data/CRDS_clean/2024_June-Sep_CRDS.P8.dat")
+piclean(input_path = "D:/Data Analysis/Gas_data/Raw_data/CRDS_raw/Picarro_G2508/2024", output_path = "D:/Data Analysis/Gas_data/Clean_data/CRDS_clean", result_file_name = "2024_Nov_06_to_11_CRDS.P8")  
+CRDS.P8 <- fread("D:/Data Analysis/Gas_data/Clean_data/CRDS_clean/2024_Nov_06_to_11_CRDS.P8.dat")
 CRDS.P8 <- CRDS.P8 %>% rename_with(~paste0(., ".P8"), -DATE.TIME) # Add Suffix
 
 #Picarro G2509 
-piclean(input_path = "D:/Data Analysis/Gas_data/Raw_data/CRDS_raw/Picarro_G2509/2024", output_path = "D:/Data Analysis/Gas_data/Clean_data/CRDS_clean", result_file_name = "2024_June-Sep_CRDS.P9")  
-CRDS.P9 <- fread("D:/Data Analysis/Gas_data/Clean_data/CRDS_clean/2024_June-Sep_CRDS.P9.dat")
+piclean(input_path = "D:/Data Analysis/Gas_data/Raw_data/CRDS_raw/Picarro_G2509/2024", output_path = "D:/Data Analysis/Gas_data/Clean_data/CRDS_clean", result_file_name = "2024_Nov_06_to_11_CRDS.P9")  
+CRDS.P9 <- fread("D:/Data Analysis/Gas_data/Clean_data/CRDS_clean/2024_Nov_06_to_11_CRDS.P9.dat")
 CRDS.P9 <- CRDS.P9 %>% rename_with(~paste0(., ".P9"), -DATE.TIME) # Add Suffix
 
 
@@ -31,8 +31,8 @@ data.table::setDT(CRDS.P9)
 
 
 # Filter rows by 'DATE.TIME' range before merging
-start_date <- as.POSIXct("2024-09-20 11:09:00")  # Set start date
-end_date <- as.POSIXct("2024-10-21 12:29:00")  # Set end date
+start_date <- as.POSIXct("2024-11-06 13:00:00")  # Set start date
+end_date <- as.POSIXct("2024-11-11 08:56:00")  # Set end date
 
 
 CRDS.P8$DATE.TIME = as.POSIXct(CRDS.P8$DATE.TIME, format = "%Y-%m-%d %H:%M:%S")
@@ -46,7 +46,7 @@ CRDS.comb <- CRDS.P8[CRDS.P9, on = "DATE.TIME", roll = "nearest"]
 
 
 # write
-write.csv(CRDS.comb, "2024_Sep_Oct_CRDS.comb.csv", row.names = FALSE) 
+write.csv(CRDS.comb, "2024_Nov_06_to_11_CRDS.comb.csv", row.names = FALSE) 
 
 
 # Summary Statistics
@@ -59,4 +59,5 @@ count_p8 <- CRDS.comb %>%
 count_p9 <- CRDS.comb %>%
         group_by(MPVPosition.P9) %>%
         summarise(count = n())
+
 
