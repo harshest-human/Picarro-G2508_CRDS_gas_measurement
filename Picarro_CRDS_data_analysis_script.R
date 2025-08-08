bc10bc9c3c60ba76c34f0d69c7be25ca3ba29cf4
Harsh Sahu <harshsahu.8085@gmail.com>
2025-08-08 02:32:16 +0200

    processing including mgm3 conversions

--- Picarro_CRDS_data_analysis_script.R
+++ Picarro_CRDS_data_analysis_script.R
@@ -1,3 +1,4 @@
+##### libraries #######
 getwd()
 library(tidyverse)
 library(reshape2)
@@ -14,217 +15,119 @@ source("Picarro_CRDS_data_cleaning_script.R")
 
 ####### ATB Data importing and cleaning ########
 #Picarro G2508
-ATB_CRDS.1 <- piclean(input_path = "D:/Data Analysis/Gas_data/Raw_data/CRDS_raw/Picarro_G2508/2025/04",
-                   
-                   output_path = "D:/Data Analysis/Gas_data/Clean_data/CRDS_clean",
-                   
-                   result_file_name = "20250408-15_Ring_7.5_cycle_ATB_CRDS.1",
-                   
+ATB_CRDS.1 <- piclean(input_path = "D:/Data Analysis/Gas_data/Raw_data/Ringversuche_2025_raw/ATB_CRDS_raw/2025",
+                      
                    gas = c("CO2", "CH4", "NH3", "H2O"),
                    
                    start_time = "2025-04-08 12:00:00",
                    
-                   end_time = "2025-04-15 12:59:59",
+                   end_time = "2025-04-15 23:59:59",
                    
                    flush = 180, # Flush time in seconds
                    
-                   interval = 450) # Total time at MPVPosition in seconds
-
+                   interval = 450,  # Total time at MPVPosition in seconds
+                   
+                   MPVPosition.levels = c("1", "2", "3"),
+                   
+                   location.levels = c("N", "in", "S"),
+                   
+                   lab = "ATB",
+                   
+                   analyzer = "CRDS.1")
 
 # Read in the data
-ATB_CRDS.1 <- read.csv("D:/Data Analysis/Gas_data/Clean_data/CRDS_clean/20250408-15_Ring_7.5_cycle_ATB_CRDS.1.csv")
-
-# Create an hourly timestamp to group by
-ATB_CRDS.1$DATE.TIME <- as.POSIXct(ATB_CRDS.1$DATE.TIME, format = "%Y-%m-%d %H:%M:%S")
-ATB_CRDS.1$DATE.TIME <- floor_date(ATB_CRDS.1$DATE.TIME, "hour")
-
-# Calculate the hourly average for each gas by MPVPosition
-ATB_avg <- ATB_CRDS.1 %>%
-        group_by(DATE.TIME, MPVPosition) %>%
-        summarise(
-                CO2 = mean(CO2, na.rm = TRUE),
-                CH4 = mean(CH4, na.rm = TRUE),
-                NH3 = mean(NH3, na.rm = TRUE),
-                H2O = mean(H2O, na.rm = TRUE),
-                .groups = "drop"  # To avoid warning about grouping
-        )%>%
-        mutate(
-                CO2 = CO2 * 37.2,    # ppm to mg/m3 for CO2
-                CH4 = CH4 * 13.6,   # ppm to mg/m3 for CH4
-                NH3 = NH3 * 14.4,  # ppm to mg/m3 for NH3
-        ) 
-
-ATB_avg <- ATB_avg %>%
-        filter(MPVPosition %in% c(1, 2, 3)) %>%
-        mutate(
-                location = recode(as.factor(MPVPosition),
-                                  `1` = "N",
-                                  `2` = "in",
-                                  `3` = "S"),
-                lab = factor("ATB"),
-                analyzer = factor("CRDS.1")
-        )
-
-# Write csv
-ATB_avg <- ATB_avg %>% select(DATE.TIME, MPVPosition, location, lab, analyzer, everything())
-ATB_avg$DATE.TIME <- format(ATB_avg$DATE.TIME, "%Y-%m-%d %H:%M:%S")
-write.csv(ATB_avg,"20250408-15_hourly_ATB_CRDS.1.csv" , row.names = FALSE, quote = FALSE)
+ATB_7.5_avg <- read.csv("20250408.1200-20250415.2359_ATB_450avg_CRDS.1.csv")
 
 # Reshape to wide format, each gas and MPVPosition combination becomes a column
-ATB_long <- ATB_avg %>%
-        select(-MPVPosition) %>%
+ATB_hourly_wide <- ATB_7.5_avg %>%
+        select(-MPVPosition, -step_id, -measuring.time) %>%
+        mutate(DATE.TIME = floor_date(as.POSIXct(DATE.TIME), unit = "hour")) %>%
+        group_by(DATE.TIME, location, lab, analyzer) %>%
+        summarise(across(where(is.numeric), ~ mean(.x, na.rm = TRUE)), .groups = "drop")%>%
         pivot_wider(
                 names_from = c(location),
-                values_from = c(CO2, CH4, NH3, H2O),
-                names_glue = "{.value}_{location}"
-        )
-
+                values_from = c("CO2_ppm", "CO2_mgm3", "CH4_ppm", "CH4_mgm3",
+                                "NH3_ppm", "NH3_mgm3", "H2O_vol"),
+                names_glue = "{.value}_{location}")
 
 # Write csv day wise
-write.csv(ATB_long,"20250408-15_long_ATB_CRDS.1.csv" , row.names = FALSE, quote = FALSE)
-
+write.csv(ATB_hourly_wide,"20250408-15_ATB_hourly_wide_CRDS.1.csv" , row.names = FALSE, quote = FALSE)
 
 ####### UB Data importing and cleaning ########
 #Picarro G2508
 UB_CRDS.2 <- piclean(input_path = "D:/Data Analysis/Gas_data/Raw_data/Ringversuche_2025_raw/UB_CRDS_raw",
-                      
-                      output_path = "D:/Data Analysis/Gas_data/Clean_data/CRDS_clean",
-                      
-                      result_file_name = "20250408-15_Ring_7.5_cycle_UB_CRDS.2",
-                      
-                      gas = c("CO2", "CH4", "NH3", "H2O"),
-                      
-                      start_time = "2025-04-08 12:00:00",
-                      
-                      end_time = "2025-04-15 12:59:59",
-                      
-                      flush = 180, # Flush time in seconds
-                      
-                      interval = 450) # Total time at MPVPosition in seconds
-
-
+                     
+                     gas = c("CO2", "CH4", "NH3", "H2O"),
+                     
+                     start_time = "2025-04-08 12:00:00",
+                     
+                     end_time = "2025-04-15 23:59:59",
+                     
+                     flush = 180, # Flush time in seconds
+                     
+                     interval = 450,  # Total time at MPVPosition in seconds
+                     
+                     MPVPosition.levels = c("8", "1", "9"),
+                     
+                     location.levels = c("N", "in", "S"),
+                     
+                     lab = "UB",
+                     
+                     analyzer = "CRDS.2")
 
 # Read in the data
-UB_CRDS.2 <- read.csv("D:/Data Analysis/Gas_data/Clean_data/CRDS_clean/20250408-15_Ring_7.5_cycle_UB_CRDS.2.csv")
-
-# Create an hourly timestamp to group by
-UB_CRDS.2$DATE.TIME <- as.POSIXct(UB_CRDS.2$DATE.TIME, format = "%Y-%m-%d %H:%M:%S")
-UB_CRDS.2$DATE.TIME <- floor_date(UB_CRDS.2$DATE.TIME, "hour")
-
-# Calculate the hourly average for each gas by MPVPosition
-UB_avg <- UB_CRDS.2 %>%
-        group_by(DATE.TIME, MPVPosition) %>%
-        summarise(
-                CO2 = mean(CO2, na.rm = TRUE),
-                CH4 = mean(CH4, na.rm = TRUE),
-                NH3 = mean(NH3, na.rm = TRUE),
-                H2O = mean(H2O, na.rm = TRUE),
-                .groups = "drop"  # To avoid warning about grouping
-        )%>%
-        mutate(
-                CO2 = CO2 * 37.2,    # ppm to mg/m3 for CO2
-                CH4 = CH4 * 13.6,   # ppm to mg/m3 for CH4
-                NH3 = NH3 * 14.4,  # ppm to mg/m3 for NH3
-        ) 
-
-UB_avg <- UB_avg %>%
-        filter(MPVPosition %in% c(1, 8, 9)) %>%
-        mutate(
-                location = recode(as.factor(MPVPosition),
-                                  `8` = "N",
-                                  `1` = "in",
-                                  `9` = "S"),
-                lab = factor("UB"),
-                analyzer = factor("CRDS.2")
-        )
-
-# Write csv
-UB_avg <- UB_avg %>% select(DATE.TIME, MPVPosition, location, lab, analyzer, everything())
-UB_avg$DATE.TIME <- format(UB_avg$DATE.TIME, "%Y-%m-%d %H:%M:%S")
-write.csv(UB_avg,"20250408-15_hourly_UB_CRDS.2.csv" , row.names = FALSE, quote = FALSE)
-
+UB_7.5_avg <- read.csv("20250408.1200-20250415.2359_UB_450avg_CRDS.2.csv")
 
 # Reshape to wide format, each gas and MPVPosition combination becomes a column
-UB_long <- UB_avg %>%
-        select(-MPVPosition) %>%
+UB_hourly_wide <- UB_7.5_avg %>%
+        select(-MPVPosition, -step_id, -measuring.time) %>%
+        mutate(DATE.TIME = floor_date(as.POSIXct(DATE.TIME), unit = "hour")) %>%
+        group_by(DATE.TIME, location, lab, analyzer) %>%
+        summarise(across(where(is.numeric), ~ mean(.x, na.rm = TRUE)), .groups = "drop")%>%
         pivot_wider(
                 names_from = c(location),
-                values_from = c(CO2, CH4, NH3, H2O),
-                names_glue = "{.value}_{location}"
-        )
-
+                values_from = c("CO2_ppm", "CO2_mgm3", "CH4_ppm", "CH4_mgm3",
+                                "NH3_ppm", "NH3_mgm3", "H2O_vol"),
+                names_glue = "{.value}_{location}")
 
 # Write csv day wise
-write.csv(UB_long,"20250408-15_long_UB_CRDS.2.csv" , row.names = FALSE, quote = FALSE)
+write.csv(UB_hourly_wide,"20250408-15_UB_hourly_wide_CRDS.2.csv" , row.names = FALSE, quote = FALSE)
 
-
-####### UB Data importing and cleaning ########
+####### LUFA Data importing and cleaning ########
 #Picarro G2508
 LUFA_CRDS.3 <- piclean(input_path = "D:/Data Analysis/Gas_data/Raw_data/Ringversuche_2025_raw/LUFA_CRDS_raw",
-                        
-                        output_path = "D:/Data Analysis/Gas_data/Clean_data/CRDS_clean",
-                        
-                        result_file_name = "20250408-15_Ring_7.5_cycle_LUFA_CRDS.3",
-                        
-                        gas = c("CO2", "CH4", "NH3", "H2O"),
-                        
-                        start_time = "2025-04-08 12:00:00",
-                        
-                        end_time = "2025-04-15 12:59:59",
-                        
-                        flush = 180, # Flush time in seconds
-                        
-                        interval = 450) # Total time at MPVPosition in seconds
-
+                       
+                       gas = c("CO2", "CH4", "NH3", "H2O"),
+                       
+                       start_time = "2025-04-08 12:00:00",
+                       
+                       end_time = "2025-04-15 23:59:59",
+                       
+                       flush = 180, # Flush time in seconds
+                       
+                       interval = 450,  # Total time at MPVPosition in seconds
+                       
+                       MPVPosition.levels = c("3", "1", "2"),
+                       
+                       location.levels = c("N", "in", "S"),
+                       
+                       lab = "LUFA",
+                       
+                       analyzer = "CRDS.3")
 
 # Read in the data
-LUFA_CRDS.3 <- read.csv("D:/Data Analysis/Gas_data/Clean_data/CRDS_clean/20250408-15_Ring_7.5_cycle_LUFA_CRDS.3.csv")
-
-# Create an hourly timestamp to group by
-LUFA_CRDS.3$DATE.TIME <- as.POSIXct(LUFA_CRDS.3$DATE.TIME, format = "%Y-%m-%d %H:%M:%S")
-LUFA_CRDS.3$DATE.TIME <- floor_date(LUFA_CRDS.3$DATE.TIME, "hour")
-
-# Calculate the hourly average for each gas by MPVPosition
-LUFA_avg <- LUFA_CRDS.3 %>%
-        group_by(DATE.TIME, MPVPosition) %>%
-        summarise(
-                CO2 = mean(CO2, na.rm = TRUE),
-                CH4 = mean(CH4, na.rm = TRUE),
-                NH3 = mean(NH3, na.rm = TRUE),
-                H2O = mean(H2O, na.rm = TRUE),
-                .groups = "drop"  # To avoid warning about grouping
-        )%>%
-        mutate(
-                CO2 = CO2 * 37.2,    # ppm to mg/m3 for CO2
-                CH4 = CH4 * 13.6,   # ppm to mg/m3 for CH4
-                NH3 = NH3 * 14.4,  # ppm to mg/m3 for NH3
-        ) 
-
-LUFA_avg <- LUFA_avg %>%
-        filter(MPVPosition %in% c(1, 2, 3)) %>%
-        mutate(
-                location = recode(as.factor(MPVPosition),
-                                  `1` = "in",
-                                  `2` = "S",
-                                  `3` = "N"),
-                lab = factor("LUFA"),
-                analyzer = factor("CRDS.3")
-        )
-
-# Write csv
-LUFA_avg <- LUFA_avg %>% select(DATE.TIME, MPVPosition, location, lab, analyzer, everything())
-LUFA_avg$DATE.TIME <- format(LUFA_avg$DATE.TIME, "%Y-%m-%d %H:%M:%S")
-write.csv(LUFA_avg,"20250408-15_hourly_LUFA_CRDS.3.csv" , row.names = FALSE, quote = FALSE)
+LUFA_7.5_avg <- read.csv("20250408.1200-20250415.2359_LUFA_450avg_CRDS.3.csv")
 
 # Reshape to wide format, each gas and MPVPosition combination becomes a column
-LUFA_long <- LUFA_avg %>%
-        select(-MPVPosition) %>%
+LUFA_hourly_wide <- LUFA_7.5_avg %>%
+        select(-MPVPosition, -step_id, -measuring.time) %>%
+        mutate(DATE.TIME = floor_date(as.POSIXct(DATE.TIME), unit = "hour")) %>%
+        group_by(DATE.TIME, location, lab, analyzer) %>%
+        summarise(across(where(is.numeric), ~ mean(.x, na.rm = TRUE)), .groups = "drop")%>%
         pivot_wider(
                 names_from = c(location),
-                values_from = c(CO2, CH4, NH3, H2O),
-                names_glue = "{.value}_{location}"
-        )
+                values_from = c("CO2_ppm", "CO2_mgm3", "CH4_ppm", "CH4_mgm3",
+                                "NH3_ppm", "NH3_mgm3", "H2O_vol"),
+                names_glue = "{.value}_{location}")
 
-
-# Write csv day wise
-write.csv(LUFA_long,"20250408-15_long_LUFA_CRDS.3.csv" , row.names = FALSE, quote = FALSE)
+write.csv(LUFA_hourly_wide,"20250408-15_LUFA_hourly_wide_CRDS.3.csv" , row.names = FALSE, quote = FALSE)
