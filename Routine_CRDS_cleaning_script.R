0c0d7a3be4aacaf0331b41bd83d450f4675ef688
Harsh Sahu <harshsahu.8085@gmail.com>
2025-09-15 15:32:53 +0200

    2025-09-05 to 2025-09-12

--- Routine_CRDS_cleaning_script.R
+++ Routine_CRDS_cleaning_script.R
@@ -1,4 +1,5 @@
 getwd()
+####### libraries ######
 library(tidyverse)
 library(reshape2)
 library(hablar)
@@ -388,7 +389,7 @@ CRDS_combined_avg <- CRDS_combined_df %>%
 
 write_excel_csv(CRDS_combined_avg, "H_CRDS_2025_08_19-2025_08_28.csv")
 
-####### 2025-08-28 to 2025-09-04 ATB Data importing and cleaning ########
+####### 2025-08-28 to 2025-09-05 ATB Data importing and cleaning ########
 CRDS9_20250828 <- piclean(input_path = "D:/Data Analysis/Gas_data/Raw_data/CRDS_raw/Picarro_G2509/2025",
                           
                           gas = c("CO2", "CH4", "NH3", "H2O"),
@@ -498,5 +499,118 @@ CRDS_in_out <- CRDS_combined_df %>%
                 names_sep = "_"
         )
 
-write_excel_csv(CRDS_in_out, "H_CRDS_2025_08_28-2025-09-04.csv")
+write_excel_csv(CRDS_in_out, "H_CRDS_2025_08_28-2025-09-05.csv")
+
+
+####### 2025-09-05 to 2025-09-12 ATB Data importing and cleaning ########
+CRDS9_20250905 <- piclean(input_path = "D:/Data Analysis/Gas_data/Raw_data/CRDS_raw/Picarro_G2509/2025",
+                          
+                          gas = c("CO2", "CH4", "NH3", "H2O"),
+                          
+                          start_time = "2025-09-05 11:34:41",
+                          
+                          end_time = "2025-09-12 10:03:57",
+                          
+                          flush = 60, # Flush time in seconds
+                          
+                          interval = 240,  # Total time at MPVPosition in seconds
+                          
+                          MPVPosition.levels = c("1", "2", "3", "4", "5", "6", "7", "8",
+                                                 "9", "10", "11", "12", "13", "14", "15", "16"),
+                          
+                          location.levels = c("1", "3", "4", "6", "7", "9", "10", "12",
+                                              "13", "15", "16", "18", "22", "24", "in", "out"),
+                          
+                          lab = "ATB",
+                          
+                          analyzer = "CRDS9")
+
+
+CRDS8_20250905 <- piclean(input_path = "D:/Data Analysis/Gas_data/Raw_data/CRDS_raw/Picarro_G2508/2025",
+                          
+                          gas = c("CO2", "CH4", "NH3", "H2O"),
+                          
+                          start_time = "2025-09-05 11:31:05",
+                          
+                          end_time = "2025-09-12 10:07:00",
+                          
+                          flush = 60, # Flush time in seconds
+                          
+                          interval = 240,  # Total time at MPVPosition in seconds
+                          
+                          MPVPosition.levels = c("1", "2", "3", "4", "5", "6", "7", "8", 
+                                                 "9", "10", "11", "12", "13", "14", "15", "16"),
+                          
+                          location.levels = c("28", "30", "31", "33", "34", "36", "37", "39",
+                                              "40", "42", "43", "45", "46", "48", "49", "51"),
+                          
+                          lab = "ATB",
+                          
+                          analyzer = "CRDS8")
+
+CRDS_combined_df <- rbind(CRDS8_20250905, CRDS9_20250905) %>% arrange(DATE.TIME) %>%
+        mutate(location = factor(location))
+
+#Data Visualization
+ggline(CRDS_combined_df, 
+       x = "location", 
+       y = "CO2",
+       add = "mean_se",         # Use mean ± SD
+       error.plot = "errorbar", # Show error bars
+       ylab = "Mean CO2 (ppm)",
+       xlab = "MPV Position",
+       title = "Mean CO2 ± SD by MPVPosition",
+       color = "steelblue",
+       add.params = list(width = 0.2)) +
+        theme_minimal()
+
+ggline(CRDS_combined_df, 
+       x = "location", 
+       y = "CH4",
+       add = "mean_se",         # Use mean ± SD
+       error.plot = "errorbar", # Show error bars
+       ylab = "Mean CH4 (ppm)",
+       xlab = "MPV Position",
+       title = "Mean CH4 ± SD by MPVPosition",
+       color = "green4",
+       add.params = list(width = 0.2)) +
+        theme_minimal()
+
+ggline(CRDS_combined_df, 
+       x = "location", 
+       y = "NH3",
+       add = "mean_se",         # Use mean ± SD
+       error.plot = "errorbar", # Show error bars
+       ylab = "Mean NH3 (ppm)",
+       xlab = "MPV Position",
+       title = "Mean NH3 ± SD by MPVPosition",
+       color = "orange4",
+       add.params = list(width = 0.2)) +
+        theme_minimal()
+
+# Calculate the hourly average of all MPVPosition
+CRDS_in_out <- CRDS_combined_df %>%
+        filter(location %in% c("in","out")) %>%
+        mutate(
+                DATE.TIME = if_else(location == "out",
+                                    DATE.TIME - seconds(450),  # shift out backwards
+                                    DATE.TIME),
+                DATE.HOUR = floor_date(DATE.TIME, "hour")
+        ) %>%
+        select(-step_id, -MPVPosition, -measuring.time) %>%
+        group_by(DATE.HOUR, location) %>%
+        summarise(
+                CO2 = mean(CO2, na.rm = TRUE),
+                CH4 = mean(CH4, na.rm = TRUE),
+                NH3 = mean(NH3, na.rm = TRUE),
+                H2O = mean(H2O, na.rm = TRUE),
+                .groups = "drop"
+        ) %>%
+        tidyr::pivot_wider(
+                names_from = location,
+                values_from = c(CO2, CH4, NH3, H2O),
+                names_sep = "_"
+        )
+
+write_excel_csv(CRDS_in_out, "H_CRDS_20250905-20250912.csv")
 
