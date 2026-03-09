e13d5b075ee92866195119596c8c2e4c3ae50b34
Harsh Sahu <harshsahu.8085@gmail.com>
2026-03-09 13:00:57 +0100

    reshape_crds new function

--- Picarro_CRDS_data_cleaning_script.R
+++ Picarro_CRDS_data_cleaning_script.R
@@ -1,14 +1,5 @@
-piclean <- function(input_path,
-                    gas,
-                    start_time,
-                    end_time,
-                    flush,
-                    interval,
-                    MPVPosition.levels = NULL,
-                    location.levels = NULL,
-                    lab = NULL,
-                    analyzer = NULL) 
-{
+
+piclean <- function(input_path,gas,start_time,end_time,flush,interval,MPVPosition.levels = NULL,location.levels = NULL,lab = NULL,analyzer = NULL) {
         library(dplyr)
         library(lubridate)
         
@@ -122,6 +113,7 @@ piclean <- function(input_path,
         return(summarized)
 }
 
+
 piconcatenate <- function(folder_path, output_csv = "combined_clean_data.csv") {
         # Load necessary package
         library(dplyr)
@@ -169,3 +161,95 @@ piconcatenate <- function(folder_path, output_csv = "combined_clean_data.csv") {
         return(all_data)
 }
 
+
+reshape_crds <- function(df_raw) {
+        
+        valid_locations <- c("in", "S", "N")
+        
+        df_hourly_long <- df_raw %>%
+                filter(location %in% valid_locations) %>%
+                mutate(DATE.HOUR = floor_date(DATE.TIME, "hour")) %>%
+                group_by(DATE.HOUR, location, lab, analyzer) %>%
+                summarise(across(c(CO2, CH4, NH3, H2O, N2O), ~mean(.x, na.rm = TRUE)),
+                          .groups = "drop")
+        
+        df_dummy <- expand_grid(
+                DATE.HOUR = seq(min(df_hourly_long$DATE.HOUR),
+                                max(df_hourly_long$DATE.HOUR),
+                                by = "hour"),
+                analyzer = unique(df_hourly_long$analyzer),
+                location = unique(df_hourly_long$location),
+                lab      = unique(df_hourly_long$lab)
+        )
+        
+        df_hourly_wide <- df_dummy %>%
+                left_join(df_hourly_long,
+                          by = c("DATE.HOUR", "location", "analyzer", "lab")) %>%
+                pivot_wider(
+                        names_from = location,
+                        values_from = c(CO2, CH4, NH3, H2O, N2O),
+                        names_sep = "_"
+                ) %>%
+                arrange(DATE.HOUR)
+        
+        make_final <- function(df_wide, suffix, site_code_label) {
+                
+                gases <- c("CO2", "CH4", "NH3", "H2O", "N2O")
+                cols <- paste0(gases, "_", suffix)
+                cols <- cols[cols %in% names(df_wide)]
+                
+                df_site <- df_wide %>%
+                        select(DATE.HOUR, analyzer, lab, all_of(cols)) %>%
+                        rename_with(~ gsub(paste0("_", suffix), "", .x), all_of(cols))
+                
+                for (g in gases) {
+                        if (!g %in% names(df_site)) df_site[[g]] <- NA_real_
+                }
+                
+                df_out <- df_site %>%
+                        mutate(
+                                LOCATION = "Gross Kreutz",
+                                SITE_CODE = site_code_label,
+                                MEASUREMENT_PERIOD = "",
+                                DATE_TIME = DATE.HOUR,
+                                MEASUREMENT_POINT_CODE = lab,
+                                GAS_MEASUREMENT_METHOD = paste0("LINE_", analyzer),
+                                CO2_DRY_PPM = CO2,
+                                CH4_DRY_PPM = CH4,
+                                NH3_DRY_PPM = NH3,
+                                H2O_VOL_PCT = H2O,
+                                N2O_DRY_PPM = N2O,
+                                NO2_DRY_PPM = "",
+                                NO_DRY_PPM = "",
+                                CO_DRY_PPM = "",
+                                COMMENTS = "",
+                                AVG_TOTAL_NUMBER_ANIMALS = "",
+                                AVG_WEIGHT_KG = "",
+                                AVG_DAYS_IN_PREGNANCY = "",
+                                AVG_MILK_YIELD_CORR_KGAD = "",
+                                IM_AIR_TEMPERATURE_C = ""
+                        ) %>%
+                        select(
+                                LOCATION, SITE_CODE, MEASUREMENT_PERIOD, DATE_TIME,
+                                MEASUREMENT_POINT_CODE, GAS_MEASUREMENT_METHOD,
+                                CO2_DRY_PPM, CH4_DRY_PPM, NH3_DRY_PPM, H2O_VOL_PCT,
+                                N2O_DRY_PPM, NO2_DRY_PPM, NO_DRY_PPM, CO_DRY_PPM,
+                                COMMENTS, AVG_TOTAL_NUMBER_ANIMALS, AVG_WEIGHT_KG,
+                                AVG_DAYS_IN_PREGNANCY, AVG_MILK_YIELD_CORR_KGAD,
+                                IM_AIR_TEMPERATURE_C
+                        ) %>%
+                        arrange(DATE_TIME)
+                
+                attr(df_out, "start_dt") <- min(df_out$DATE_TIME, na.rm = TRUE)
+                attr(df_out, "end_dt")   <- max(df_out$DATE_TIME, na.rm = TRUE)
+                
+                return(df_out)
+        }
+        
+        return(list(
+                hourly_wide = df_hourly_wide,
+                final_IN = make_final(df_hourly_wide, "in", "IN"),
+                final_OUT_South = make_final(df_hourly_wide, "S", "OUT_South"),
+                final_OUT_North = make_final(df_hourly_wide, "N", "OUT_North")
+        ))
+}
