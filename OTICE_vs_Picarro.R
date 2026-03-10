036dfe6a1549521117642851f18a5e3583291b5d
Harsh Sahu <harshsahu.8085@gmail.com>
2026-03-10 18:08:52 +0100

    simple comparison by bindrows

--- OTICE_vs_Picarro.R
+++ OTICE_vs_Picarro.R
@@ -93,175 +93,50 @@ OTICE_data <- bind_rows(
 write.csv(OTICE_data, "OTICE_high_res_20250930_20251231.csv", row.names = FALSE)
                 
 ###### Comparison ######
-library(dplyr)
-library(stringr)
-library(lubridate)
-library(readr)
-library(readxl)
-library(purrr)
-library(ggplot2)
-
-#-----------------------------
 # Read files
-#-----------------------------
 otice <- read_csv("OTICE_high_res_20250930_20251231.csv", show_col_types = FALSE)
 crds  <- read_csv("CRDS8+9_high_res_20250925_20251231.csv", show_col_types = FALSE)
 meta  <- read_excel("OTICE_meta.xlsx")
 
-#-----------------------------
+
 # Clean OTICE
-#-----------------------------
 otice2 <- otice %>%
-        mutate(
-                DATE.HOUR = as.character(DATE.HOUR),
-                DATE.HOUR = if_else(
-                        str_detect(DATE.HOUR, "^\\d{4}-\\d{2}-\\d{2}$"),
-                        paste0(DATE.HOUR, " 00:00:00"),
-                        DATE.HOUR
-                ),
-                DATE.HOUR = ymd_hms(DATE.HOUR, tz = "UTC"),
-                OTICE_location = as.integer(str_remove(location, "^O"))
+        transmute(
+                CO2_raw,
+                NH3_raw,
+                DATE.HOUR,
+                analyzer = as.character(analyzer),
+                location = str_remove(as.character(location), "^O"),
+                location = str_pad(location, width = 2, side = "left", pad = "0"),
+                location = paste("OTICE", location, sep = "_")
         ) %>%
-        select(DATE.HOUR, analyzer, location, OTICE_location, CO2_raw, NH3_raw, CO2_corr, NH3_corr)
+        select(DATE.HOUR, location, CO2_raw, NH3_raw)
 
-#-----------------------------
 # Clean CRDS
-#-----------------------------
 crds2 <- crds %>%
-        mutate(
-                DATE.HOUR = as.character(DATE.HOUR),
-                DATE.HOUR = if_else(
-                        str_detect(DATE.HOUR, "^\\d{4}-\\d{2}-\\d{2}$"),
-                        paste0(DATE.HOUR, " 00:00:00"),
-                        DATE.HOUR
-                ),
-                DATE.HOUR = ymd_hms(DATE.HOUR, tz = "UTC"),
-                CRDS_location = as.integer(str_extract(as.character(location), "\\d+")),
-                CRDS_analyzer = as.character(analyzer)
-        ) %>%
-        select(DATE.HOUR, analyzer, CRDS_analyzer, location, CRDS_location, CO2_raw, NH3_raw)
-
-#-----------------------------
-# Clean metadata
-#-----------------------------
-meta2 <- meta %>%
-        mutate(
-                DATE.HOUR_start = as.character(DATE.HOUR_start),
-                DATE.HOUR_end   = as.character(DATE.HOUR_end),
-                
-                DATE.HOUR_start = if_else(
-                        str_detect(DATE.HOUR_start, "^\\d{4}-\\d{2}-\\d{2}$"),
-                        paste0(DATE.HOUR_start, " 00:00:00"),
-                        DATE.HOUR_start
-                ),
-                DATE.HOUR_end = if_else(
-                        str_detect(DATE.HOUR_end, "^\\d{4}-\\d{2}-\\d{2}$"),
-                        paste0(DATE.HOUR_end, " 23:59:59"),
-                        DATE.HOUR_end
-                ),
-                
-                DATE.HOUR_start = ymd_hms(DATE.HOUR_start, tz = "UTC"),
-                DATE.HOUR_end   = ymd_hms(DATE.HOUR_end, tz = "UTC"),
-                
-                OTICE_location = as.integer(OTICE_location),
-                CRDS_location  = as.integer(CRDS_location),
-                CRDS_analyzer  = as.character(CRDS_analyzer)
+        transmute(
+                DATE.HOUR,
+                location = paste(as.character(analyzer),
+                                 as.character(location),
+                                 sep = "_"),
+                CO2_raw,
+                NH3_raw
         )
 
-make_period_data <- function(i, meta_tbl, otice_tbl, crds_tbl) {
-        
-        m <- meta_tbl[i, ]
-        
-        ot_sub <- otice_tbl %>%
-                filter(
-                        OTICE_location == m$OTICE_location,
-                        DATE.HOUR >= m$DATE.HOUR_start,
-                        DATE.HOUR <= m$DATE.HOUR_end
-                ) %>%
-                arrange(DATE.HOUR)
-        
-        cr_sub <- crds_tbl %>%
-                filter(
-                        CRDS_location == m$CRDS_location,
-                        CRDS_analyzer == m$CRDS_analyzer,
-                        DATE.HOUR >= m$DATE.HOUR_start,
-                        DATE.HOUR <= m$DATE.HOUR_end
-                ) %>%
-                arrange(DATE.HOUR)
-        
-        list(
-                meta = m,
-                otice = ot_sub,
-                crds = cr_sub
+OTICE_CRDS <- bind_rows(otice2, crds2) %>%
+        arrange(DATE.HOUR)
+
+write.csv(OTICE_CRDS, "OTICE_CRDS_row_bind_20250925_20251231.csv", row.names = FALSE)
+
+
+###### Data Visualization ######
+df_plot <- OTICE_CRDS %>%
+        filter(
+                DATE.HOUR >= as.POSIXct("2025-09-29 00:00:00"),
+                DATE.HOUR <= as.POSIXct("2025-10-06 23:59:59"),
+                location %in% c("OTICE_02", "CRDS8_14")
         )
-}
 
-period_list <- map(seq_len(nrow(meta2)), make_period_data,
-                   meta_tbl = meta2, otice_tbl = otice2, crds_tbl = crds2)
-
-period_summary <- map_dfr(seq_along(period_list), function(i) {
-        
-        x <- period_list[[i]]
-        m <- x$meta
-        
-        tibble(
-                period_index    = i,
-                OTICE_location  = m$OTICE_location,
-                CRDS_location   = m$CRDS_location,
-                CRDS_analyzer   = m$CRDS_analyzer,
-                start           = m$DATE.HOUR_start,
-                end             = m$DATE.HOUR_end,
-                otice_n         = nrow(x$otice),
-                crds_n          = nrow(x$crds),
-                co2_otice_n     = sum(!is.na(x$otice$CO2_raw)),
-                co2_crds_n      = sum(!is.na(x$crds$CO2_raw)),
-                nh3_otice_n     = sum(!is.na(x$otice$NH3_raw)),
-                nh3_crds_n      = sum(!is.na(x$crds$NH3_raw))
-        )
-})
-
-plot_period_ts <- function(period_obj, gas = c("CO2_raw", "NH3_raw")) {
-        
-        gas <- match.arg(gas)
-        
-        m   <- period_obj$meta
-        ot  <- period_obj$otice
-        cr  <- period_obj$crds
-        
-        ot_plot <- ot %>%
-                transmute(
-                        DATE.HOUR = DATE.HOUR,
-                        source = "OTICE",
-                        value = .data[[gas]]
-                )
-        
-        cr_plot <- cr %>%
-                transmute(
-                        DATE.HOUR = DATE.HOUR,
-                        source = "CRDS",
-                        value = .data[[gas]]
-                )
-        
-        plot_df <- bind_rows(ot_plot, cr_plot)
-        
-        ggplot(plot_df, aes(x = DATE.HOUR, y = value, color = source)) +
-                geom_line(na.rm = FALSE) +
-                theme_bw() +
-                labs(
-                        title = paste0(
-                                gas, " | OTICE O", sprintf("%02d", m$OTICE_location),
-                                " vs ", m$CRDS_analyzer, " L", m$CRDS_location
-                        ),
-                        subtitle = paste0(
-                                format(m$DATE.HOUR_start, "%Y-%m-%d %H:%M:%S"),
-                                " to ",
-                                format(m$DATE.HOUR_end, "%Y-%m-%d %H:%M:%S")
-                        ),
-                        x = "Date-Time",
-                        y = gas,
-                        color = NULL
-                )
-}
-
-plot_period_ts(period_list[[1]], gas = "CO2_raw")
-plot_period_ts(period_list[[1]], gas = "NH3_raw")
+ggplot(df_plot, aes(x = DATE.HOUR, y = CO2_raw, color = location)) +
+        geom_line() +
+        theme_bw()
